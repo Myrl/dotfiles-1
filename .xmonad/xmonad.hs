0143ec25cec6d60b072b890e6eb4473d68dad24b
Nikolay Amiantov <ab@fmap.me>
2016-10-14 01:24:30 +0300

    Revert "bigger dpi"
    This reverts commit b089dd71b540ed7622704b7819306cb24de08f65.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -43,9 +43,9 @@ myConfig =
   where sc = ewmh $ pagerHints xfceConfig
 
 myXPConfig = def
-  { font = "xft:Source Code Pro:size=14"
+  { font = "xft:Source Code Pro:size=8"
   , bgColor = "black"
-  , height = 36
+  , height = 20
   , searchPredicate = isInfixOf
   }
 
